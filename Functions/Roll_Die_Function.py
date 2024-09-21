ff5eb51352cdeb3102094a5251cedf086d0010f1
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-09-21 14:15:35 +1000

    removed repeating lines of code in the advantage/disadvantage file

--- Functions/Roll_Die_Function.py
+++ Functions/Roll_Die_Function.py
@@ -3,19 +3,18 @@ from colored import Fore, Style
 def __init__(self, value):
        self.value=value
 def roll(self):
-        return random.randint(1,self.value)
-def advantage():
+       return random.randint(1,self.value)
+def roll_two_dice():
     firstd20=random.randint(1, 20)
     secondd20=random.randint(1, 20)
     dice_list=[firstd20, secondd20]
-    print(dice_list)
     dice_list.sort()
+    print(dice_list)
+    return dice_list
+def advantage():
+    dice_list=roll_two_dice()
     print("Highest D20 roll is:",f"{Fore.blue}{dice_list[1]}{Style.reset}\n")
 
 def disadvantage():
-    firstd20=random.randint(1, 20)
-    secondd20=random.randint(1, 20)
-    dice_list=[firstd20, secondd20]
-    print(dice_list)
-    dice_list.sort()
+    dice_list=roll_two_dice()
     print("Lowest D20 roll is:",f"{Fore.red}{dice_list[0]}{Style.reset}\n")
