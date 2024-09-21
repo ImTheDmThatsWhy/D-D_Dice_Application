0d53c7901cc216e3e4aa7cacd584a046147e0091
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-09-21 17:57:20 +1000

    dice combo menu created

--- DiceCombos.py
+++ DiceCombos.py
@@ -1,4 +1,18 @@
+from Classes.Dice import roll_dice
 def dice_combination_menu():
-    print ("Are you rolling a dice combo or not?")
-    print("Type yes to roll combo, type no to continue to dice roll")
+    print ("Enter 1 to roll a saved dice combination")
+    print("Enter 2 to proceed to roll dice menu")
+    print("Enter 3 to exit")
     choice = input("Enter your answer:\n")
+
+choice=""
+while choice!="3":
+    if choice =="1":
+        print("Enter the name of the saved combination you wish to roll eg fireball")
+    elif choice == "2":
+        print("Proceeding to dice rolling menu")
+        roll_dice()
+    elif choice == "3":
+        print("Exiting to main menu")
+    else:
+        print("Invalid input please try again")
