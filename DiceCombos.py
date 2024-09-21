b4b500cbe4cadcc1ca5174fcf3da2d66896b6692
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-09-21 17:48:03 +1000

    created a function for dice combo menu

---
+++ DiceCombos.py
@@ -0,0 +1,4 @@
+def dice_combination_menu():
+    print ("Are you rolling a dice combo or not?")
+    print("Type yes to roll combo, type no to continue to dice roll")
+    choice = input("Enter your answer:\n")
